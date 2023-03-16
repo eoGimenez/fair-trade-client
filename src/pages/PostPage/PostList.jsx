1c070f41423be852c3ee03068dd956a53e4b8e12
lucasgonzalezgagliardo <lucas.e.gonzalez@gmail.com>
2023-03-16 16:26:34 +0100

    funciona postlists

--- src/pages/PostPage/PostList.jsx
+++ src/pages/PostPage/PostList.jsx
@@ -1,16 +1,16 @@
 import Navbar2 from "../../components/Navbar/Navbar2";
 import Post from "../../components/Post";
-//import Categorias from "../../components/Posts/Categorias";
 
 
 function PostList() {
 
   return (
-    <>      
+    <> 
+    <div className="divRow">
     <Navbar2/>
       <Post />
-    
-    </>
+      </div>
+         </>
   );
 }
 
